caeea298e5a6458631fb07f66e44d2a93a13f5fd
Vincenzolaveglia <vincenzolaveglia10@gmail.com>
2024-02-26 17:34:02 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PagamentoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gestionale_Edile.Controllers
+{
+    public class PagamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
